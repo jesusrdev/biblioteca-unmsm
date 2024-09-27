2552c3645e83308b04b79e0f2cb3fcdebab3f599
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-27 16:43:03 -0500

    adding initial backend

---
+++ backend/src/test/java/org/cibertec/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
