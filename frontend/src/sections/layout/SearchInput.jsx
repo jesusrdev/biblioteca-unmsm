b042b398dbf5e397edbb4b479b2c0f9eb0a899af
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-02 17:59:20 -0500

    adding icon to search input

--- frontend/src/sections/layout/SearchInput.jsx
+++ frontend/src/sections/layout/SearchInput.jsx
@@ -1,13 +1,16 @@
 import { IconButton, InputBase, Paper } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import Image from "next/image";
 
 export default function SearchInput() {
   return (
     <Paper className="px-3 rounded-md shadow-none bg-search-input">
-      <IconButton type="button" className="p-1 m-1" aria-label="search">
-        <SearchIcon />
+      <IconButton type="button" className="p-[2px] m-[2px] mr-1" aria-label="search">
+        <Image src="/icons/search.svg" alt="search" width={28} height={28} />
       </IconButton>
-      <InputBase placeholder="Busca tu libro..." className="*:placeholder:italic text-base font-medium *:placeholder:font-normal" />
+      <InputBase
+        placeholder="Busca tu libro..."
+        className="*:placeholder:italic text-base font-medium *:placeholder:font-normal"
+      />
     </Paper>
-  )
+  );
 }
