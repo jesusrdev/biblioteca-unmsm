a20ae75a907532cb3d4ee4a55b5ed67abcb7e1cf
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-02 22:43:43 -0500

    adding background gradient to card book

--- frontend/src/sections/books/CardBook.jsx
+++ frontend/src/sections/books/CardBook.jsx
@@ -1,18 +1,32 @@
+"use client";
 import CustomCard from "@/components/Card";
-import { Button, CardContent, CardHeader } from "@mui/material";
-import React from "react";
+import { Button, CardContent } from "@mui/material";
+import { useEffect, useState } from "react";
+import { usePalette } from 'color-thief-react';
 
 export default function CardBook({ book }) {
+  const [colors, setColors] = useState(["#ffffff", "#ff3300", "#ffffff"]);
+  const { data: color, loading, error } = usePalette(book.image, 3, 'hex', { crossOrigin: 'anonymous', quality: 1 });
+
+  useEffect(() => {
+    if (color) {
+      setColors(color);
+    }
+  }, [color]);
+
   return (
     <Button className="h-full p-0 m-auto w-fit">
       <CustomCard className="h-full px-0 py-0 m-0">
-        <div className="p-5 bg-orange-400">
+        <div
+          className="p-5"
+          style={{
+            background: `linear-gradient(45deg, ${colors[1]} 0%, white 130%)`,
+          }}
+        >
           <img
             src={book.image}
             alt="book"
-            className=" h-44 border-[4px] border-white border-solid [box-shadow:7px_7px_6px_1px_rgba(0,0,0,0.25)]"
-            // width={200}
-            // height={300}
+            className="h-44 border-[4px] border-white border-solid [box-shadow:7px_7px_6px_1px_rgba(0,0,0,0.25)]"
           />
         </div>
         <CardContent
