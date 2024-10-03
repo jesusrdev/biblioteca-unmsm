439d7d53db8fd7d5788ab326abb773d034ec3077
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-03 16:05:28 -0500

    refactor de image of card book to their own component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -21,7 +21,7 @@ const books = [
   {
     title: "La vida es un libro",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeD40YKDr0JEKMyMhxoftS3gbXaAtszj_O5A&s",
+      "https://www.sancristoballibros.com/imagenes/9789972/978997233516.GIF",
     author: {
       name: "Carmen Fuentes",
     },
@@ -29,7 +29,7 @@ const books = [
   {
     title: "El libro de la vida",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeD40YKDr0JEKMyMhxoftS3gbXaAtszj_O5A&s",
+      "https://d20f60vzbd93dl.cloudfront.net/uploads/tienda_010118/tienda_010118_8b6ec482638a397f49146caaa8a8c02f945ab468_producto_large_90.jpg?not-from-cache-please",
     author: {
       name: "Carlos Fuentes",
     },
