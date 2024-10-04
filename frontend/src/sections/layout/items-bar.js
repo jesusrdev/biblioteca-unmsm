76a6418dfb90814be3f68f3bcc8531f4cb4d345f
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-10-04 17:58:23 -0500

    adding admin button and LeftBar component

--- frontend/src/sections/layout/items-bar.js
+++ frontend/src/sections/layout/items-bar.js
@@ -1,8 +1,8 @@
 export const principalList = [
-  { text: "Inicio", icon: "/icons/home.svg" },
-  { text: "Categoría", icon: "/icons/book.svg" },
-  { text: "Mis Préstamos", icon: "/icons/open-book.svg" },
-  { text: "Favoritos", icon: "/icons/heart.svg" },
+  { text: "Inicio", icon: "/icons/home.svg", url: "/" },
+  { text: "Categoría", icon: "/icons/book.svg", url: "/category" },
+  { text: "Mis Préstamos", icon: "/icons/open-book.svg", url: "/loans" },
+  { text: "Favoritos", icon: "/icons/heart.svg", url: "/favorites" },
 ];
 
 export const sectionList = [
